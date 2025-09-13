8e518721e8f750953964f9a902877db19a14dc3c
STRO09 <sagarjanjoted123@gmail.com>
2025-09-13 11:45:35 +0530

    added db-prod.properties

--- backend/src/test/java/unittests/Tests.java
+++ backend/src/test/java/unittests/Tests.java
@@ -12,32 +12,32 @@ import models.User;
 
 
 public class Tests {
-
-	Session session;
-	UserDAO dao = new UserImplementor();
-
-//	@BeforeEach
-//	void open() {
-//		session = HibernateUtil.getSession();
-//	}
 //
-//	@AfterEach
-//	void close() {
-//		if (session != null)
-//			session.close();
+//	Session session;
+//	UserDAO dao = new UserImplementor();
 //
+////	@BeforeEach
+////	void open() {
+////		session = HibernateUtil.getSession();
+////	}
+////
+////	@AfterEach
+////	void close() {
+////		if (session != null)
+////			session.close();
+////
+////	}
+////	
+//	@Test
+//	void save_fetch_User() {
+//
+//		User u = new User("testuser", "tu@example.com", "hash", "Test User", User.Role.DEVELOPER);
+//        boolean status = dao.registerUser(u);
+//        assertTrue(status, "User Registration Test passed");
+//        
+//		User fetcheduser = dao.getUserById(u.getId());
+//		assertEquals("Test User", fetcheduser.getFullName());
+//        
 //	}
-//	
-	@Test
-	void save_fetch_User() {
-
-		User u = new User("testuser", "tu@example.com", "hash", "Test User", User.Role.DEVELOPER);
-        boolean status = dao.registerUser(u);
-        assertTrue(status, "User Registration Test passed");
-        
-		User fetcheduser = dao.getUserById(u.getId());
-		assertEquals("Test User", fetcheduser.getFullName());
-        
-	}
 
 }
